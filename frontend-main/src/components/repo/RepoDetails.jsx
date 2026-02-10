e2ec8a0a5f4c2b5d684fff05f88282abd4686963
Vinayak45541 <vinayakpattar980@gmail.com>
2026-02-10 10:16:42 +0530

    fix case-sensitive css filename for render build

--- frontend-main/src/components/repo/RepoDetails.jsx
+++ frontend-main/src/components/repo/RepoDetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../Navbar";
 import API from "../../api/axios";
-import "./repodetails.css";
+import "./RepoDetails.css";
 
 const RepoDetails = () => {
   const { id } = useParams();
