bbb4e8a887600ee3ff44bf5053d50f6a525f51f2
Yuvaraj M N <yuvarajmn737@gmail.com>
2026-02-15 20:48:31 +0530

    save error

--- backend-main/middleware/authMiddleware.js
+++ backend-main/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ function authMiddleware(req, res, next) {
   try {
     const decoded = jwt.verify(
       token.replace("Bearer ", ""),
-      process.env.JWT_SECRET_KEY
+      process.env.JWT_SECRET
     );
 
     req.user = decoded; // { id: userId }
